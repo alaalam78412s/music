6a7f0fbe97de0505e4fcc59f881d4dd3f0c3bc6a
alaalam78412s <127143813+alaalam78412s@users.noreply.github.com>
2023-04-15 22:56:38 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,21 +2,11 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
-	res.send('Hello Express app!');
-});
-
-app.listen(3000, () => {
-	console.log('\033[32m server started');
-});
-
-const Discord = require('discord.js');
-const client = new Discord.Client();
  const { joinVoiceChannel } = require('@discordjs/voice');
 client.on('ready', () => {
     
     setInterval( async () => {
-    client.channels.fetch("ايدي الاتشانل") 
+    client.channels.fetch("1094309879621099561") 
      .then((channel) => { 
       const VoiceConnection = joinVoiceChannel({
        channelId: channel.id, 
